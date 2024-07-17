42421981568aab41fa561237e774e341c2d6c4c4
darkelvesasd <96123329+darkelvesasd@users.noreply.github.com>
2024-07-17 13:35:26 +0800

    Create 27. 移除元素.cpp（解法二）

---
+++ 27. 移除元素.cpp
@@ -0,0 +1,16 @@
+int removeElement(int* nums, int numsSize, int val) {
+    	int src = 0;
+	int dst = 0;
+	while (src < numsSize)
+	{
+		if (nums[src] != val)
+		{
+			nums[dst++] = nums[src++];
+		}
+		else
+		{
+			src++;
+		}
+	}
+	return dst;
+}
